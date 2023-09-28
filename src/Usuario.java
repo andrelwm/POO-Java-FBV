30a751f2eac3bd4af12092d80eca8d88a55ccf04
André Melo <102316139+andrelwm@users.noreply.github.com>
2023-09-28 10:18:18 -0300

    Update Usuario.java
    Criados os Get's e Set's da classe

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,21 +1,50 @@
 
 public class Usuario {
 
-    int cod_usuario;
-    String  email;
-    String  senha;
-    String  nome;
-    String  regiao;
-    String  telefone;
-
-    private Usuario (String email, String senha, String nome, String regiao, String telefone) {
-
+    private int cod_usuario;
+    private String  nm_usuario, email, senha, nome, regiao, telefone;
+    
+    public String getNm_usuario() {
+        return nm_usuario;
+    }
+    public void setNm_usuario(String nm_usuario) {
+        this.nm_usuario = nm_usuario;
+    }
+    public int getCod_usuario() {
+        return cod_usuario;
+    }
+    public void setCod_usuario(int cod_usuario) {
+        this.cod_usuario = cod_usuario;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
         this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
         this.senha = senha;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+    public String getRegiao() {
+        return regiao;
+    }
+    public void setRegiao(String regiao) {
         this.regiao = regiao;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
         this.telefone = telefone;
-
     }
 
 }
